d612f15b5eb551da78b58ea36181078d22f92dce
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-12-01 16:07:45 +0530

    Add todo adding.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import TodoList from './containers/TodoList';
 
+const baseUrl = 'https://mytodo-server.now.sh';
+
 class App extends Component {
   addItem() {
+    const newItem = this.refs.newItem.value;
+    this.refs.newItem.value = 'adding...';
 
+    fetch(`${baseUrl}/add`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: newItem }),
+    })
+      .then((res) => res.json())
+      .then(() => { this.refs.newItem.value = '' })
+      .catch((err) => alert(err.message));
   }
 
   removeItem(item) {
