70a04a8d12e5b1b435d7af2aad1e39c1563dc008
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-12-01 12:56:24 +0530

    Use states to keep items.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 
 class App extends Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      items: [
+        'Learn React',
+        'Learn Redux',
+        'Create Sample App',
+      ]
+    };
+  }
+
   render() {
+    const { items } = this.state;
+
     return (
       <div>
         <h1>My Todo App</h1>
         <ul>
-          <TodoItem name="Learn React"/>
-          <TodoItem name="Learn Redux"/>
-          <TodoItem name="Create Sample App"/>
+          { items.map((item) => (
+            <TodoItem key={item} name={item} />
+          )) }
         </ul>
       </div>
     );
